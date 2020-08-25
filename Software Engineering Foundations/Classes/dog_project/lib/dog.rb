fd9ece126e360bbac31b3149793c1e7bcd44ea22
Gavi Schneider <gavischneider@gmail.com>
2020-08-26 00:05:48 +0300

    added dog_project

---
+++ Software Engineering Foundations/Classes/dog_project/lib/dog.rb
@@ -0,0 +1,46 @@
+class Dog
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age
+    end
+
+    def age=(num)
+        @age = num
+    end
+
+    def bark
+        if self.age > 3
+            return @bark.upcase
+        else
+            return @bark.downcase
+        end
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end
+
+    def favorite_food?(food)
+        if self.favorite_foods.include?(food) || self.favorite_foods.include?(food.capitalize)
+            return true
+        else
+            return false
+        end
+    end
+
+end
